34b7456a7463c490f707ca31f20aabe31cf9e0e8
245967906 <245967906@qq.com>
2017-12-28 16:20:38 +0800

    使用v-for指令渲染商品列表

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,22 @@
 var vm = new Vue({
   el: '#app',
   data: {
-
+    productList: [],
+    totalMoney: 0,
   },
   mounted: function() {
-
+    this.cartView();
   },
   filters: {
 
   },
   methods: {
-
+    cartView: function() {
+      // then()方法声明当前面的函数执行加载完毕后, 再执行then()里面的代码
+      this.$http.get("data/cartData.json").then(function(res) {
+        this.productList = res.data.result.list;
+        this.totalMoney = res.data.result.totalMoney;
+      });
+    },
   },
 })
